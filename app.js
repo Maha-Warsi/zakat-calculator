48485c18f0fa089490c37d4fb2754bc467a49df8
Maha Warsi <125538345+Maha-Warsi@users.noreply.github.com>
2023-04-06 07:19:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+// making zakat calculator: 
+
+var zakatPercentage = .025;
+var userInput = prompt("enter your value");
+var result = Number(userInput)*zakatPercentage;
+var ZakatResult = ("your zakat value is : Rs ");
+alert(ZakatResult + result)
+  
+
+
+
